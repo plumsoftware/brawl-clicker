464b4ba022d3b622a6b06888ea1c94d8e5122e65
Slava <83229808+plumsoftware@users.noreply.github.com>
2024-12-16 18:50:03 +0600

    app (#2)
    [+] Добавил баннер снизу страницы.

--- app/src/main/java/ru/plumsoftware/brawlstarsclicker/activities/MainActivity.java
+++ app/src/main/java/ru/plumsoftware/brawlstarsclicker/activities/MainActivity.java
@@ -13,6 +13,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
+import android.util.DisplayMetrics;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
@@ -27,6 +28,9 @@ import com.yandex.mobile.ads.appopenad.AppOpenAd;
 import com.yandex.mobile.ads.appopenad.AppOpenAdEventListener;
 import com.yandex.mobile.ads.appopenad.AppOpenAdLoadListener;
 import com.yandex.mobile.ads.appopenad.AppOpenAdLoader;
+import com.yandex.mobile.ads.banner.BannerAdEventListener;
+import com.yandex.mobile.ads.banner.BannerAdSize;
+import com.yandex.mobile.ads.banner.BannerAdView;
 import com.yandex.mobile.ads.common.AdError;
 import com.yandex.mobile.ads.common.AdRequest;
 import com.yandex.mobile.ads.common.AdRequestConfiguration;
@@ -60,6 +64,8 @@ public class MainActivity extends AppCompatActivity {
     private final String AD_UNIT_ID = "R-M-2428506-3";
     private final AdRequestConfiguration adRequestConfiguration = new AdRequestConfiguration.Builder(AD_UNIT_ID).build();
     private AppOpenAd mAppOpenAd = null;
+    private final String bannerId = "R-M-2428506-4";
+    private final double TABLET_SCREEN_SIZE_THRESHOLD = 7.0;
 
 
     @SuppressLint("SetTextI18n")
@@ -89,6 +95,61 @@ public class MainActivity extends AppCompatActivity {
         MobileAds.initialize(this, () -> {
 
         });
+
+        BannerAdView mBannerAdView = (BannerAdView) findViewById(R.id.ad_banner_view);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        WindowManager windowManager = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        windowManager.getDefaultDisplay().getMetrics(displayMetrics);
+        int screenWidth = displayMetrics.widthPixels;
+        int screenHeight = displayMetrics.heightPixels;
+
+        double screenInches = Math.sqrt(Math.pow(screenWidth / displayMetrics.xdpi, 2) +
+                Math.pow(screenHeight / displayMetrics.ydpi, 2));
+
+        int bannerHeight;
+        if (screenInches >= TABLET_SCREEN_SIZE_THRESHOLD) {
+            bannerHeight = (int) (screenHeight * 0.08);
+        } else {
+            bannerHeight = (int) (screenHeight * 0.036);
+        }
+
+        mBannerAdView.setAdUnitId(bannerId); //RuStore
+        mBannerAdView.setAdSize(BannerAdSize.inlineSize(MainActivity.this, screenWidth, bannerHeight));
+        final AdRequest adRequestB = new AdRequest.Builder().build();
+        mBannerAdView.setBannerAdEventListener(new BannerAdEventListener() {
+            @Override
+            public void onAdLoaded() {
+
+            }
+
+            @Override
+            public void onAdFailedToLoad(@NonNull AdRequestError adRequestError) {
+
+            }
+
+            @Override
+            public void onAdClicked() {
+
+            }
+
+            @Override
+            public void onLeftApplication() {
+
+            }
+
+            @Override
+            public void onReturnedToApplication() {
+
+            }
+
+            @Override
+            public void onImpression(@Nullable ImpressionData impressionData) {
+
+            }
+        });
+
+        // Загрузка объявления.
+        mBannerAdView.loadAd(adRequestB);
 
         if (getIntent().getBooleanExtra("soa", true)) {
             appOpenAdLoader = new AppOpenAdLoader(context);
